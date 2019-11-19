7e10b972b6e57c26fd7f4cc528a4e4c82b54f280
Cooler Zhang 🐾 <cooler.zhang@serko.com>
2019-11-19 14:37:05 +0800

    Init Project

---
+++ ConsoleAppTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = new List<string>() { "a", "b", "c" };
+            items.ForEach(a =>
+            {
+                if (a == "b")
+                    return;
+                Console.WriteLine(a);
+            });
+            Console.ReadLine();
+        }
+    }
+}
